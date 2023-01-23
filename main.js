8e0ac788ff322775bddf9b5f4f14e33b085591fa
muhammadalkurdy <jobirtolaganov59@gmail.com>
2023-01-23 17:06:33 +0500

    canculater 2

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-let answerEl = document.querySelector("#javob");
+// let answerEl = document.querySelector("#javob");
 
-answerEl.addEventListener("click", () => {
-        let inp1 = parseInt(document.querySelector("#inp1").value)
-        let inp2 = parseInt(document.querySelector("#inp2").value)
-        let p = document.querySelector("#p");
-        let m = document.querySelector("#m");
-        let u = document.querySelector("#u");
-        let r = document.querySelector("#r");
+// answerEl.addEventListener("click", () => {
+//         let inp1 = parseInt(document.querySelector("#inp1").value)
+//         let inp2 = parseInt(document.querySelector("#inp2").value)
+//         let p = document.querySelector("#p");
+//         let m = document.querySelector("#m");
+//         let u = document.querySelector("#u");
+//         let r = document.querySelector("#r");
 
-        if (p.checked) {
-            answerEl.innerHTML = inp1 + inp2 
-        }
-        if (m.checked) {
-            let min = inp1 - inp2
-            answerEl.innerHTML = "Javob: <b>" + min + "</b>"
-        }
-        if (u.checked) {
-            answerEl.innerHTML = "Javob: <b>" + inp1 * inp2 + "</b>"
-        }
-        if (r.checked) {
-            answerEl.innerHTML = "Javob: <b>" + inp1 / inp2 + "</b>"
-        }
+//         if (p.checked) {
+//             answerEl.innerHTML = inp1 + inp2 
+//         }
+//         if (m.checked) {
+//             let min = inp1 - inp2
+//             answerEl.innerHTML = "Javob: <b>" + min + "</b>"
+//         }
+//         if (u.checked) {
+//             answerEl.innerHTML = "Javob: <b>" + inp1 * inp2 + "</b>"
+//         }
+//         if (r.checked) {
+//             answerEl.innerHTML = "Javob: <b>" + inp1 / inp2 + "</b>"
+//         }
 
-        console.log(inp1);
-    })
+//         console.log(inp1);
+//     })
+
+
+
